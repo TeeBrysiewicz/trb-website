6a0fef2368fe2e24f812fab9f3b477f1f363cfba
Tobias Brysiewicz <tbrysiewicz@att.net>
2021-08-13 14:20:03 -0500

    mobile menu functionality and wip on mobile styles, currently at skills

--- app/pods/components/navigation/component.js
+++ app/pods/components/navigation/component.js
@@ -3,4 +3,5 @@ import { tracked } from '@glimmer/tracking';
 
 export default class NavigationComponent extends Component {
   @tracked isOpen = false;
+  sections = ['about', 'experience', 'education', 'skills', 'portfolio', 'interests', 'contact'];
 }
