76ef61b2e45a4ba6552be1406587bc280c43505e
Tobias Brysiewicz <tbrysiewicz@att.net>
2021-11-19 09:12:31 -0600

    auto open nav on portfolio items

--- app/pods/components/items-view/component.js
+++ app/pods/components/items-view/component.js
@@ -3,7 +3,7 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class ItemsViewComponent extends Component {
-  @tracked isOpen = false;
+  @tracked isOpen = true;
 
   @action
   toggleMenu() {
